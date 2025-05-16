9bed58411bcf4f7284a9cc0bcd3b4ea479649e59
kelvin_marques <kelvin.max.adm@gmail.com>
2025-05-16 13:06:52 -0300

    (feat):criando UsersDto

--- src/main/java/com/minha_carteira/carteira_finaceira/services/UsersService.java
+++ src/main/java/com/minha_carteira/carteira_finaceira/services/UsersService.java
@@ -1,12 +1,15 @@
 package com.minha_carteira.carteira_finaceira.services;
 
 import com.minha_carteira.carteira_finaceira.Repositories.UsersRepository;
+import com.minha_carteira.carteira_finaceira.dto.UsersDto;
 import com.minha_carteira.carteira_finaceira.entities.UsersEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UsersService {
@@ -14,7 +17,16 @@ public class UsersService {
     private UsersRepository usersRepository;
 
     @Transactional(readOnly = true) //-> Otimizar as operações de leitura.
-    public List<UsersEntity> findAll(){
-        return usersRepository.findAll();
+    public List<UsersDto> findAll(){
+        List<UsersEntity> list = usersRepository.findAll();
+        return list.stream().map(x -> new UsersDto(x)).collect(Collectors.toList());
+
+
+//sem expressão lambda
+//        List<UsersDto> listDto = new ArrayList<>();
+//        for (UsersEntity user : list ){
+//            listDto.add(new UsersDto(user));
+//        }
+//        return listDto;
     }
 }
