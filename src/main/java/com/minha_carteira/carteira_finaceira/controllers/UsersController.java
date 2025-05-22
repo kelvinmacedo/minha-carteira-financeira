32557bca1b90a51e0bc40caf3e28cef9fcb979cc
kelvin_marques <kelvin.max.adm@gmail.com>
2025-05-21 23:23:10 -0300

    (feat): Adicionado exception, findById e insert Users!

--- src/main/java/com/minha_carteira/carteira_finaceira/controllers/UsersController.java
+++ src/main/java/com/minha_carteira/carteira_finaceira/controllers/UsersController.java
@@ -3,11 +3,12 @@ package com.minha_carteira.carteira_finaceira.controllers;
 import com.minha_carteira.carteira_finaceira.dto.UsersDto;
 import com.minha_carteira.carteira_finaceira.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,5 +22,19 @@ public class UsersController {
     public ResponseEntity<List<UsersDto>> findAll(){
         List<UsersDto> list = usersService.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UsersDto> findById(@PathVariable Long id){
+        UsersDto userDto= usersService.findById(id);
+        return ResponseEntity.ok().body(userDto);
+    }
+
+    @PostMapping(value = "/insert")
+    public ResponseEntity<UsersDto> insert(@RequestBody UsersDto usersDto) {
+        usersDto = usersService.insert(usersDto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/id")
+                .buildAndExpand(usersDto.getId()).toUri();
+        return ResponseEntity.created(uri).body(usersDto);
     }
 }
--- src/main/java/com/minha_carteira/carteira_finaceira/services/UsersService.java
+++ src/main/java/com/minha_carteira/carteira_finaceira/services/UsersService.java
@@ -3,12 +3,14 @@ package com.minha_carteira.carteira_finaceira.services;
 import com.minha_carteira.carteira_finaceira.Repositories.UsersRepository;
 import com.minha_carteira.carteira_finaceira.dto.UsersDto;
 import com.minha_carteira.carteira_finaceira.entities.UsersEntity;
+import com.minha_carteira.carteira_finaceira.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -16,7 +18,7 @@ public class UsersService {
     @Autowired
     private UsersRepository usersRepository;
 
-    @Transactional(readOnly = true) //-> Otimizar as operações de leitura.
+    @Transactional(readOnly = true)
     public List<UsersDto> findAll(){
         List<UsersEntity> list = usersRepository.findAll();
         return list.stream().map(x -> new UsersDto(x)).collect(Collectors.toList());
@@ -28,5 +30,25 @@ public class UsersService {
 //            listDto.add(new UsersDto(user));
 //        }
 //        return listDto;
+    }
+    @Transactional(readOnly = true)
+    public UsersDto findById(Long id) {
+        Optional<UsersEntity> obj = usersRepository.findById(id);
+        UsersEntity entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+        return new UsersDto(entity);
+    }
+
+    @Transactional
+    public UsersDto insert(UsersDto usersDto) {
+        UsersEntity entity = new UsersEntity();
+        entity.setName(usersDto.getName());
+        entity.setEmail(usersDto.getEmail());
+        entity.setPassword(usersDto.getPassword());
+        entity = usersRepository.save(entity);
+        UsersDto user = new UsersDto(entity.getId(), entity.getName(), usersDto.getEmail());
+        user.setId(entity.getId());
+        user.setEmail(entity.getEmail());
+        user.setName(entity.getName());
+        return user;
     }
 }
